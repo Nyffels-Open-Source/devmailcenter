317adabc29b7a2cb351680934428a421ac7bc867
Chesney Nyffels <chesney.nyffels@accentjobs.be>
2024-11-12 15:22:40 +0100

    Added types for mailserver and mailserversettings

---
+++ api/DevMailCenter.Models/MailServer.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevMailCenter.Models;
+
+[Table("MailServer")]
+public class MailServer
+{
+    [Column("ServerId")]
+    public Guid Id { get; set; }
+    
+    [Column("ServerName")]
+    public string Name { get; set; }
+    
+    [Column("ServerType")]
+    public MailServerType Type { get; set; }
+
+    [Column("ServerActive")]
+    public bool Active { get; set; }
+    
+    [Column("ServerCreated")]
+    public DateTime Created { get; set; }
+    
+    [Column("ServerModified")]
+    public DateTime Modified { get; set; }
+    
+    [Column("ServerLastUsed")]
+    public DateTime LastUsed { get; set; }
+}
+
+public enum MailServerType
+{
+    Smtp = 0,
+    MicrosoftExchange = 1
+}
---
+++ api/DevMailCenter.Models/MailServerSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevMailCenter.Models;
+
+[Table("MailServerSettings")]
+public class MailServerSettings
+{
+    [Column("ServerSettingsGuid")]
+    public Guid Id { get; set; }
+    
+    [Column("ServerSettingsKey")]
+    public string Key { get; set; }
+    
+    [Column("ServerSettingsValue")]
+    public string Value { get; set; }
+    
+    [Column("MailServerSettingsCreated")]
+    public DateTime Created { get; set; }
+    
+    [Column("MailServerSettingsModified")]
+    public DateTime Modified { get; set; }
+}
