dbfbafe9d8cd66b1dfbc118e88bd9854183d28d6
Chesney Nyffels <chesney@nyffels.be>
2024-11-26 00:10:48 +0100

    Fix for multiple microsoft api requests

--- DevMailCenter.Client/src/app/portal/mailserver/add/add.component.ts
+++ DevMailCenter.Client/src/app/portal/mailserver/add/add.component.ts
@@ -7,10 +7,10 @@ import {FormsModule} from '@angular/forms';
 import {MicrosoftService} from '../../../core/services/microsoft.service';
 
 @Component({
-    selector: 'dmc-mailserver-add',
-    imports: [CardModule, CommonModule, FormsModule],
-    templateUrl: './add.component.html',
-    styleUrl: './add.component.scss'
+  selector: 'dmc-mailserver-add',
+  imports: [CardModule, CommonModule, FormsModule],
+  templateUrl: './add.component.html',
+  styleUrl: './add.component.scss'
 })
 export class AddComponent implements OnInit {
   private destroy$ = new Subject<void>();
@@ -18,31 +18,33 @@ export class AddComponent implements OnInit {
   providersLoaded = false;
   providers: string[] = [];
 
-  providerCards: {provider: string, name?: string, logoUrl?: string}[] = [];
+  providerCards: { provider: string, name?: string, logoUrl?: string }[] = [];
 
   selectedProvider: string | null = null;
 
   constructor(private configClient: ConfigClient, private microsoftService: MicrosoftService) {}
 
   ngOnInit() {
-    this.configClient.listEnableProviders().pipe(takeUntil(this.destroy$)).subscribe({
-      next: providers => {
-        this.providers = providers;
-        this.generateProviderCards();
-        this.providersLoaded = true;
-      },
-      error: error => {
-        // TODO Error handling toast. Do not show add!
-      }
-    })
+    this.configClient.listEnableProviders()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: providers => {
+          this.providers = providers;
+          this.generateProviderCards();
+          this.providersLoaded = true;
+        },
+        error: error => {
+          // TODO Error handling toast. Do not show add!
+        }
+      })
   }
 
   generateProviderCards() {
     this.providerCards = this.providers.map(provider => {
       return {
         provider: provider,
-        name: {'Smtp': 'Basic SMTP', 'MicrosoftExchange': 'Microsoft Exchange 365 / Outlook.com', 'Google': 'Gmail' }[provider] ?? 'Unknown provider',
-        logoUrl: {'Smtp': 'providers/smtp.png', 'MicrosoftExchange': 'providers/exchange.png', 'Google': 'providers/google.png' }[provider] ?? "",
+        name: {'Smtp': 'Basic SMTP', 'MicrosoftExchange': 'Microsoft Exchange 365 / Outlook.com', 'Google': 'Gmail'}[provider] ?? 'Unknown provider',
+        logoUrl: {'Smtp': 'providers/smtp.png', 'MicrosoftExchange': 'providers/exchange.png', 'Google': 'providers/google.png'}[provider] ?? "",
       }
     })
   }
@@ -56,7 +58,12 @@ export class AddComponent implements OnInit {
         break;
       }
       case 'MicrosoftExchange': {
-        // TODO
+        Object.keys(sessionStorage)
+          .filter(k => /^msal\..*/.test(k))
+          .reduce(function (obj: any, key: string) {
+            sessionStorage.removeItem(key);
+          }, {});
+
         await this.microsoftService.acquireConsentAndAuthorizationTokenByRedirect();
         break;
       }
