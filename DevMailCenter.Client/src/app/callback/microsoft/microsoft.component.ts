dbd6a4c80b548f7644e6fc9bfaed4cc4355265c8
Chesney Nyffels <chesney.nyffels@accentjobs.be>
2024-11-25 12:55:33 +0100

    Added style of exchange callback

--- DevMailCenter.Client/src/app/callback/microsoft/microsoft.component.ts
+++ DevMailCenter.Client/src/app/callback/microsoft/microsoft.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ProgressBarModule } from 'primeng/progressbar';
 import {CommonModule} from '@angular/common';
+import {CardModule} from 'primeng/card';
 
 @Component({
     selector: 'dmc-callback-microsoft',
     imports: [
       CommonModule,
-      ProgressBarModule
+      ProgressBarModule,
+      CardModule
     ],
     templateUrl: './microsoft.component.html',
     styleUrl: './microsoft.component.scss'
 })
-export class MicrosoftComponent {
+export class MicrosoftComponent implements OnInit {
+  state: "process" | 'error' | 'success' = 'process';
 
+  constructor() {
+  }
+
+  ngOnInit() {}
 }
