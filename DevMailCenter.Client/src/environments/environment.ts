80ee7c8247d0eabb84d733a6a3928e5de7e17677
Chesney Nyffels <chesney@nyffels.be>
2024-11-26 13:41:16 +0100

    Updated environment to new logic

--- DevMailCenter.Client/src/environments/environment.ts
+++ DevMailCenter.Client/src/environments/environment.ts
@@ -3,10 +3,4 @@ export const environment = {
   api: {
     url: "{{API_URL}}"
   },
-  microsoft: {
-    clientId: "{{MICROSOFT_CLIENT_ID}}",
-    redirectUri: "https://localhost:4200/callback/microsoft",
-    authority: "https://login.microsoftonline.com/common/",
-    scopes: ["{{MICROSOFT_SCOPES}}"]
-  }
 };
