90d3b230333e149bccd13a6f8adc1acf5a5da641
Chesney Nyffels <chesney@nyffels.be>
2024-11-26 18:55:31 +0100

    Made it possible to delete mailservers

--- DevMailCenter.Client/src/app/portal/mailserver/list/list.component.ts
+++ DevMailCenter.Client/src/app/portal/mailserver/list/list.component.ts
@@ -9,6 +9,9 @@ import {CardModule} from 'primeng/card';
 import {CommonModule, DatePipe} from '@angular/common';
 import {TagModule} from 'primeng/tag';
 import {RippleModule} from 'primeng/ripple';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ToastModule} from 'primeng/toast';
 
 @Component({
   selector: 'dmc-mailserver-list',
@@ -20,10 +23,12 @@ import {RippleModule} from 'primeng/ripple';
     TooltipModule,
     CardModule,
     DatePipe,
-    TagModule
+    TagModule,
+    ConfirmDialogModule
   ],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
+  providers: [ConfirmationService]
 })
 export class ListComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
@@ -31,7 +36,7 @@ export class ListComponent implements OnInit, OnDestroy {
   servers: MailServer[] = [];
   isLoading = false;
 
-  constructor(private mailServerClient: MailServerClient, private router: Router, private activatedRoute: ActivatedRoute) {
+  constructor(private mailServerClient: MailServerClient, private router: Router, private activatedRoute: ActivatedRoute, private confirmationService: ConfirmationService) {
   }
 
   ngOnInit() {
@@ -69,9 +74,26 @@ export class ListComponent implements OnInit, OnDestroy {
   }
 
   deleteServer(server: MailServer) {
-    // TODO Are you sure you wish to delete this mailserver. All e-mails connected to this mailserver will also be deleted.
-    // TODO Delete the server
-    alert("Not yet implemented");
+    this.confirmationService.confirm({
+      message: `Are you sure you want to delete '${server.name}'?`,
+      header: 'Are you sure?',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: "none",
+      acceptButtonStyleClass: "p-button-danger",
+      rejectIcon: "none",
+      rejectButtonStyleClass: "p-button-text p-button-secondary",
+      closeOnEscape: false,
+      accept: () => {
+        this.mailServerClient.deleteMailServer(server.id)
+          .pipe(takeUntil(this.destroy$))
+          .subscribe({
+            next: () => {
+              this.servers.splice(this.servers.indexOf(server), 1);
+            }
+          })
+      },
+      key: 'positionDialog'
+    });
   }
 
   viewServer(server: MailServer) {
