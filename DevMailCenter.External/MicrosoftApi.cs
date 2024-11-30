a19afd4d9ba757b2dc4b90718fff395b01507f36
Chesney Nyffels <chesney@nyffels.be>
2024-11-30 15:14:44 +0100

    Added fetching new tokens from microsoft

--- DevMailCenter.External/MicrosoftApi.cs
+++ DevMailCenter.External/MicrosoftApi.cs
@@ -9,8 +9,7 @@ public interface IMicrosoftApi
 {
     string GenerateAuthenticationRedirectUrl(string redirectUri);
     MicrosoftTokens GetTokensByOnBehalfAccessToken(string accessToken);
-    // Task<MicrosoftTokens> GetTokensByRefreshToken(string refreshToken);
-    // Task<MicrosoftTokens> GetTokensByRefreshTokenInternal(string refreshToken);
+    MicrosoftTokens GetTokensByRefreshToken(string refreshToken);
 }
 
 public class MicrosoftApi : IMicrosoftApi
@@ -31,7 +30,7 @@ public class MicrosoftApi : IMicrosoftApi
         var clientId = this._configuration["Microsoft:Authentication:ClientId"];
         var scope = this._configuration["Microsoft:Scope"];
         var authority = "https://login.microsoftonline.com/common/";
-        
+
         string url = $"{authority}/oauth2/v2.0/authorize?";
         url += $"client_id={clientId}";
         url += $"&scope={scope}";
@@ -39,16 +38,16 @@ public class MicrosoftApi : IMicrosoftApi
         url += $"&response_mode=fragment";
         url += $"&response_type=token";
         url += $"&prompt=consent";
-        
+
         return url;
     }
-    
+
     public MicrosoftTokens GetTokensByOnBehalfAccessToken(string accessToken)
     {
         var clientId = this._configuration["Microsoft:Mailer:ClientId"];
         var clientSecret = this._configuration["Microsoft:Mailer:ClientSecret"];
         var scope = this._configuration["Microsoft:Scope"];
-        var redirectUri = "http://localhost:4200/callback/microsoft";
+        var redirectUri = "http://localhost:4200/callback/microsoft"; // TODO 
 
         string url = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
         string queryString = $"grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&client_id={clientId}&client_secret={clientSecret}&assertion={accessToken}&scope={scope}&requested_token_use=on_behalf_of";
@@ -73,54 +72,33 @@ public class MicrosoftApi : IMicrosoftApi
         }
     }
 
-    // public async Task<MicrosoftTokens> GetTokensByRefreshToken(string refreshToken)
-    // {
-    //     MailTokens accessToken = null;
-    //
-    //     if (!_cache.TryGetValue<MailTokens>(refreshToken, out accessToken))
-    //     {
-    //         var tokens = await GetTokensByRefreshTokenInternal(refreshToken);
-    //
-    //         var options = new MemoryCacheEntryOptions()
-    //             .SetAbsoluteExpiration(TimeSpan.FromSeconds(tokens.ExpiresIn - 300));
-    //
-    //         _cache.Set(refreshToken, tokens);
-    //
-    //         accessToken = tokens;
-    //     }
-    //
-    //     return accessToken;
-    // }
-    //
-    // private async Task<MicrosoftTokens> GetTokensByRefreshTokenInternal(string refreshToken)
-    // {
-    //     var clientId = this._configuration["Exchange:GraphApi:ClientId"];
-    //     var clientSecret = this._configuration["Exchange:GraphApi:ClientSecret"];
-    //     // var scope = this._configuration["Exchange:GraphApi:Scope"];
-    //     var scope = "offline_access openid profile email Mail.Send";
-    //
-    //     const string grant_type = "refresh_token";
-    //
-    //     string url = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
-    //     string queryString = $"grant_type={grant_type}&client_id={clientId}&client_secret={clientSecret}&refresh_token={refreshToken}&scope={scope}";
-    //
-    //     StringContent httpContent = new StringContent(queryString, System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
-    //     var res = await this._httpClient.PostAsync(url, httpContent);
-    //     var deserializedObject = JsonConvert.DeserializeObject<dynamic>(res.Content.ReadAsStringAsync().Result);
-    //
-    //     if (deserializedObject.ContainsKey("error"))
-    //     {
-    //         throw new Exception(deserializedObject.error_description.Value);
-    //     }
-    //     else
-    //     {
-    //         var tokens = new MailTokens()
-    //         {
-    //             AccessToken = deserializedObject.access_token,
-    //             RefreshToken = deserializedObject.refresh_token,
-    //             ExpiresIn = deserializedObject.expires_in
-    //         };
-    //         return tokens;
-    //     }
-    // }
+
+    public MicrosoftTokens GetTokensByRefreshToken(string refreshToken)
+    {
+        var clientId = this._configuration["Microsoft:Mailer:ClientId"];
+        var clientSecret = this._configuration["Microsoft:Mailer:ClientSecret"];
+        var scope = this._configuration["Microsoft:Scope"];
+
+        string url = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+        string queryString = $"grant_type=refresh_token&client_id={clientId}&client_secret={clientSecret}&refresh_token={refreshToken}&scope={scope}";
+
+        StringContent httpContent = new StringContent(queryString, System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
+        var res = _httpClient.PostAsync(url, httpContent).Result;
+        var deserializedObject = JsonConvert.DeserializeObject<dynamic>(res.Content.ReadAsStringAsync().Result);
+
+        if (deserializedObject.ContainsKey("error"))
+        {
+            throw new Exception(deserializedObject.error_description.Value);
+        }
+        else
+        {
+            var tokens = new MicrosoftTokens()
+            {
+                AccessToken = deserializedObject.access_token,
+                RefreshToken = deserializedObject.refresh_token,
+                ExpiresIn = deserializedObject.expires_in
+            };
+            return tokens;
+        }
+    }
 }
--- DevMailCenter.Logic/MicrosoftLogic.cs
+++ DevMailCenter.Logic/MicrosoftLogic.cs
@@ -1,4 +1,5 @@
-﻿using DevMailCenter.Models;
+﻿using DevMailCenter.External;
+using DevMailCenter.Models;
 using DevMailCenter.Repository;
 using DevMailCenter.Security;
 using Microsoft.Extensions.Logging;
@@ -16,17 +17,22 @@ public class MicrosoftLogic : IMicrosoftLogic
     private readonly IEmailRepository _emailRepository;
     private readonly IEmailTransactionRepository _emailTransactionRepository;
     private readonly IEncryptionLogic _encryptionLogic;
+    private readonly IMicrosoftApi _microsoftApi;
     
-    public MicrosoftLogic(ILogger<MicrosoftLogic> logger, IEmailRepository emailRepository, IEmailTransactionRepository emailTransactionRepository, IEncryptionLogic encryptionLogic)
+    public MicrosoftLogic(ILogger<MicrosoftLogic> logger, IEmailRepository emailRepository, IEmailTransactionRepository emailTransactionRepository, IEncryptionLogic encryptionLogic, IMicrosoftApi microsoftApi)
     {
         _logger = logger;
         _emailRepository = emailRepository;
         _emailTransactionRepository = emailTransactionRepository;
         _encryptionLogic = encryptionLogic;
+        _microsoftApi = microsoftApi;
     }
 
     public Guid Send(MicrosoftSettings settings, Email email)
     {
+        var refreshToken = _encryptionLogic.Decrypt(settings.RefreshToken);
+        var newTokens = _microsoftApi.GetTokensByRefreshToken(refreshToken);
+        
         // TODO
         return Guid.NewGuid();
     }
